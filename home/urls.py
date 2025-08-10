bf989d7cc129e16a46da78525a688863e99d6822
Praveen7-C <nagaraju736881@gmail.com>
2025-08-10 21:31:27 +0530

    27716

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('menu/', menu_api, name='menu_api'),
+    path('api/menu/', menu_api, name='menu_api'),
     path('',home, name= 'home'),
+    path('about/', about_us, name='about_us'),
+    path('staff/login/', staff_login, name='staff-login')
 ]
