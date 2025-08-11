fb51a31923b5876379258ee83462bca4a438de5c
Praveen7-C <nagaraju736881@gmail.com>
2025-08-12 00:20:27 +0530

    27715

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Resturant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(blanks=True , null = True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
+    metadata = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Restaurants"
