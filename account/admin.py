2c7766a0a4dccf87762a0443c5cf43716933b469
Praveen7-C <nagaraju736881@gmail.com>
2025-08-18 02:57:59 +0530

    33143

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Profile
+from django.contrib.auth.models import User
+
 # Register your models here.
-admin.site.register(UserProfile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'phone_number')
+    search_fields = ('user_username', 'full_name', 'phone_number')
