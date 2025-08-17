78fa07e489bed9342ba140819b9bf298f46e2fc3
Praveen7-C <nagaraju736881@gmail.com>
2025-08-18 03:04:53 +0530

    33143

--- account/models.py
+++ account/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20)
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, related_name='profile')
+    full_name= models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
-        return self.user.username
+        return f"Profile for {self.user.username}"
         
