4215ce7935b919027abc87f5985bdfd8c7d891cb
Praveen7-C <nagaraju736881@gmail.com>
2025-08-17 22:11:20 +0530

    33146

--- home/views.py
+++ home/views.py
@@ -1,34 +1,58 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import MenuItem, Restaurant
+from django.conf import settings
 
-@api_view(['GET'])
-def menu_api(request):
-    menu=[
-        {"name"}: "Paneer Butter Masala", "description": "Creamy paneer curry", "price":250},
-        {"name":"Veg Biryani", "description": "Spicyrice with veggies","price":200},
-        {"name": "Dal Makhani", "description":"Rich lentil curry","price":180},
-    ]
-    return Response(menu)
-
-def home(request):
-    menu = [
+def home_view(request):
+    menu_items = []
+    items = MenuItem.objects.filter(is_available=True).order_by('created_at')
+    if not items:
+        menu_items = [
         {"name": "Panner Butter Masala", "description": "Creamy panner curry", "price":250},
         {"name":"Veg Biryani", "description": "Spicy rice with veggies", "price":200},
         {"name":"Dal Makhani", "description":  "Rich lentil curry", "price":180},   
     ]
+
+    else:
+        for item in items:
+            menu_items.append({
+                "name": item.name,
+                "description": items.description,
+                "price": item.price,
+                "is_available": item.is_available
+            })
+
+    restaurant_obj = Restaurant.objects.first()
+    if restaurant_obj:
+        restaurant_name = restaurant_obj.name
+        phone_number = restaurant_obj.phone_number or getattr(settings, 'RESTAURANT_PHONE', 'Not available')
+    else:
+        restaurant_name= getattr(settings, 'RESTAURANT_NAME', 'My Restaurant')
+        phone_number = getattr(settings, 'RESTAURANT_PHONE', 'Not available')
+
     context ={
-        'restaurant_name': getattr(ssettings, 'RESTAURANT_NAME', 'Our Restaurant')
+        "menu_items": menu_items,
+        'restaurant_name': restaurant_name,
+        'phone_number': phone_number,
     }
-    return render(request, 'menu/home.html',{'menu':menu}, context)
+    return render(request, 'menu/home.html', context)
 
-
-
+#Simple About Us Page.
 def about_us(request):
-    return render(request,'menu/about.hthml')
+    restaurant_obj = Restaurant.objects.first()
+    about_text = {
+        f"Welcome to {restaurant_obj.name}. We serve delicious food!"
+        "Welcome! We are a small family-run restaurant serving tasty meals."
+    }
+    context = {
+        'about_text': about_text,
+        'restaurant_name': restaurant_obj.name if restaurant_obj else getattr(settings, 'RESTAURANT_NAME', 'My Restaurant'),
 
-def custom_404(request, exception):
+    }
+    return render(request,'menu/about.html', context)
+
+def custom_404(request):
     return render(request,'404.html', status=404)
+
 
 
     
