829b2c3f3e10930b77f8d55dd82c49df6d7433ec
Praveen7-C <nagaraju736881@gmail.com>
2025-08-19 00:00:31 +0530

    33143

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Customer
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Customer)
