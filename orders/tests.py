971c8a8b27507d91117e4e3c3da5bfbf86c296b5
Praveen7-C <nagaraju736881@gmail.com>
2025-08-18 21:31:51 +0530

    33143

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .models import Customer
 # Create your tests here.
+class CustomerModelTest(TestCase):
+    def test_create_guest_customer(self):
+        customer = Customer.objects.create(name="Test Guest", phone="1234567890")
+        self.assertEqual(customer.name,"Test Guest")
+
+        
