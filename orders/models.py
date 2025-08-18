4b82a6f34b592a5c1ba76758ac1863b9eba131d9
Praveen7-C <nagaraju736881@gmail.com>
2025-08-18 21:26:50 +0530

    33143

--- orders/models.py
+++ orders/models.py
@@ -3,20 +3,33 @@ from django.conf import settings
 from products.models import MenuItem, Restaurant
 
 # Create your models here.
+class customer(models.model):
+    name = models.CharField(max_length=255, blank=True, null =True)
+    phone = models.CharField(max_length=20, blank =True, null=True)
+    email = models.EmailField(blank=True, null-True)
+    created_at = models.DataTimeField(auto_now_add=True)
 
-
-
+    def __str__(self):
+        return self.name or f"Customer #{self.id}"
+        
 class Order(models.Model):
     STATUS_CHOICES = [
         ('pending', 'Pending'),
         ('completed', 'Completed'),
         ('cancelled','Cancelled'),
     ]
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_items = models.ManyToManyField(MenuItem)
-    total_amount = models.DecimalField(max_digits=8, decimal_places=2)
-    order_status = models.CharField(max_length=20, choices= STATUS_CHOICES, default= 'pending')
+
+    customer_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete = models.SET_NULL, blank=True, null=True 
+    )
+    customer = models.ForeignKey(
+        Customer, on_delete= models.SET_NULL, blank=True, null=True
+    )
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
+    items = models.ManyToManyField(MenuItem)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES, default= 'pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Order #{self.id} by {self.customer.username}"
+        return f"Order #{self.id} - {self.status}"
