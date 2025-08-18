956540fd9ec9ee0a01b4409403b13f3c64a320e5
Praveen7-C <nagaraju736881@gmail.com>
2025-08-19 02:12:13 +0530

    33143

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import *
+from . import views
+
+app_name = 'products'
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
+    #simple hardcoded menu
+    path('menu/simple/', views.simple_menu, name='simple-menu'),
+
+    #Restaurants CRUD
+    path('restaurants/', views.restaurant_list_create, name='restaurant-list-create'),
+    path('restaurants/<int:pk>/', views.restaurant_detail, name='restaurant-detail'),
+
+    #MenuItem CRUD
+    path('menuitems/', views.menuitem_list_create, name='menuitem-list-create'),
+    path('menuitems/<int:pk>/', views.menuitem_detail, name='menuitem-detail'),
 ]
