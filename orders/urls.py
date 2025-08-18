80c08ea834977d77703201ae4102567882b5964d
Praveen7-C <nagaraju736881@gmail.com>
2025-08-18 21:00:15 +0530

    33143

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import CustomerListCreateView, OrderCreateView, CustomerListCreateView
 
 urlpatterns = [
-    
+    path('customers/', CustomerListCreateView.as_view(), name='customer-list-create'),
+    path('orders/create/', OrderCreateView.as_view(), name='order-create')
+    path('Orders/CustomerOrderListView().as_view()', name='customer-orders'), 
 ]
