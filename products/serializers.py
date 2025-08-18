2621f1b5b329feb62760dfe44c55888f11889190
Praveen7-C <nagaraju736881@gmail.com>
2025-08-19 02:04:15 +0530

    33143

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Item
+from .models import *
 
-class ItemSerializer(serializers.ModelSerializer):
+class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
+        model = Restaurant
         fields = '__all__'
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    restaurant = serializers.PrimaryKeyRelatedField(queryset= Restaurant.objects.all())
+
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+
+
