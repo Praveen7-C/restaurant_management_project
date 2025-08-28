9667ed12d903b436b7f4df7b60345bbd2eb20a6a
Praveen7-C <nagaraju736881@gmail.com>
2025-08-28 20:16:09 +0530

    38596

--- home/models.py
+++ home/models.py
@@ -16,3 +16,19 @@ class Resturant(models.Model):
 
     class Meta:
         verbose_name_plural = "Restaurants"
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"
+
+class Feedback(models.model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
